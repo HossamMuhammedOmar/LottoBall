5425796da39717ed126b9627ba774a0254c8057f
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-09-06 19:06:17 +0200

    Updates

---
+++ precache-manifest.6cd3a10eed1c0571ac3a82b3dd41aedf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7c91ad1a12230a2c68312b3c6bfec0",
+    "url": "/LottoBall/index.html"
+  },
+  {
+    "revision": "2f16ea1b8edff680b5d9",
+    "url": "/LottoBall/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82bd8df0e43fa03b581c",
+    "url": "/LottoBall/static/js/2.1aeb62eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LottoBall/static/js/2.1aeb62eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f16ea1b8edff680b5d9",
+    "url": "/LottoBall/static/js/main.a04c6981.chunk.js"
+  },
+  {
+    "revision": "ece9905b4ea812bcebf5",
+    "url": "/LottoBall/static/js/runtime-main.0cc0ab58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LottoBall/static/media/logo.5d5d9eef.svg"
+  }
+]);
